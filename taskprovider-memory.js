52a4b7327bb291fcd85257c2e47080241110b19b
John Doe <john@doe.com>
2014-02-11 07:51:41 +0200

    First commit

---
+++ taskprovider-memory.js
@@ -0,0 +1,32 @@
+var taskCounter = 1;
+
+TaskProvider = function() {};
+TaskProvider.prototype.data = [];
+
+TaskProvider.prototype.getAll = function(callback) {
+    callback(null, this.data);
+};
+
+TaskProvider.prototype.save = function(tasks, callback) {
+    var task = null;
+    
+    for (var i = 0; i < tasks.length; i++) {
+        task = tasks[i];
+        task._id = taskCounter++;
+        task.creation_date = new Date();
+        
+        this.data.push(task);
+    }
+    
+    callback(null, tasks);
+};
+
+defaultTasks = [
+{content: "Complete this project", owner: "Sammy Brister", isCompleted: false, due_date: new Date(2014, 3, 1) },
+{content: "Finish your homework", owner: "Sammy Brister", isCompleted: false, due_date: new Date(2014, 2, 28) },
+{content: "Go to sleep", owner: "Sammy Brister", isCompleted: false, due_date: new Date() }
+];
+
+new TaskProvider().save(defaultTasks, function(){});
+
+exports.TaskProvider = TaskProvider;
