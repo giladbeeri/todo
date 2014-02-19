4380e7f6ba7937b9de7141b536c53dd9f1fb2fa0
John Doe <john@doe.com>
2014-02-19 09:33:58 +0200

    adding a checkmark that doesn't do anything right now

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -13,6 +13,8 @@ todoControllers.controller('TaskCtrl', ['$scope', '$http', function($scope, $htt
     
     $scope.deleteTask = function(taskId) {
         console.log("Angular: delete task #" + taskId);
+        // Possible to remove the task from the current array, instead of reading the data from the server again.
+        // It would be faster, but less reliable in a multi-client environment.
         $http.delete('/todo/' + taskId).success(function(data) {
             $scope.tasks = data;
         });  
