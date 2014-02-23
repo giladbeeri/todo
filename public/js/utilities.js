1dbb311a88ecd91d8fc384abda67e62be57e14ce
John Doe <john@doe.com>
2014-02-23 16:14:51 +0200

    default value of date - today

--- public/js/utilities.js
+++ public/js/utilities.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-     $('.removeTask').click(function() {
-         var id = $(this).parent().attr('id');
-         console.log(id);
-     });
+    var date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+    if (mm < 10)
+        mm = '0' + mm;
+    if (dd < 10)
+        dd = '0' + dd;
+    var today = yyyy + '-' + mm + '-' + dd;
+    console.log(today);
+    $('#newTaskDueDate').val(today);
 });
