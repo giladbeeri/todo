284acc2f831bcfccd17e6b5078e43aa7318546b6
John Doe <john@doe.com>
2014-02-26 11:19:41 +0200

    no buttons for the editable owner

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 var todoApp = angular.module('todoApp', ['xeditable', 'todoControllers']);
+
+todoApp.run(function(editableOptions, editableThemes) {
+    editableThemes.bs3.inputClass = 'input-sm';
+    editableThemes.bs3.buttonsClass = 'btn-sm';
+    editableOptions.theme = 'bs3';
+});
