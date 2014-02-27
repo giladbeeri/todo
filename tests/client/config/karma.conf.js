1218460ac8745bf2e978f91a83872ced680b5a7d
John Doe <john@doe.com>
2014-02-27 14:41:56 +0200

    Chrome -> Firefox (much faster when not in focus)

--- tests/client/config/karma.conf.js
+++ tests/client/config/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Firefox'],
 
     plugins : [
             'karma-junit-reporter',
