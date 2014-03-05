fb9698d1a17853731ef7fce989bbb97b12cefc5d
John Doe <john@doe.com>
2014-03-05 15:06:40 +0200

    owner select now uses angular-ui-select2

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var todoApp = angular.module('todoApp', ['xeditable', 'todoControllers']);
+var todoApp = angular.module('todoApp', ['ui.select2', 'xeditable', 'todoControllers']);
 
 todoApp.constant('config', {
     taskListUrl: '/tasks/'
