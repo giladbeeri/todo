92713200d7d3f380029bbb111d56a92e263157ec
John Doe <john@doe.com>
2014-03-06 17:34:39 +0200

    test-server now also sets lower priority and affinity

--- tests/scripts/test-server.bat
+++ tests/scripts/test-server.bat
@@ -1,1 +1,2 @@
-jasmine-node ..\server --autotest --watch ..\..\ --captureExceptions
+start /affinity 0xaa cmd /c "jasmine-node ..\server --autotest --watch ..\..\ --captureExceptions --color --verbose"
+wmic process where name="node.exe" CALL setpriority "idle"
