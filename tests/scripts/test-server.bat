141ce1e9969f920bc722ad6cfcd39df2ed74be96
John Doe <john@doe.com>
2014-03-09 19:41:05 +0200

    better makefile

--- tests/scripts/test-server.bat
+++ tests/scripts/test-server.bat
@@ -1,2 +1,3 @@
+; Depracted since the move to mocha (from jasmine-node). use "make test-w".
 start /affinity 0xaa cmd /c "mocha ..\server --autotest --watch ..\..\ --captureExceptions --color --verbose --check-leaks --slow 75"
 wmic process where name="node.exe" CALL setpriority "idle"
