0283c43e6063e9fc49b95109a399d7a4569a6d23
John Doe <john@doe.com>
2014-03-09 18:48:48 +0200

    test order after mocha

--- tests/scripts/test-server.bat
+++ tests/scripts/test-server.bat
@@ -1,2 +1,2 @@
-start /affinity 0xaa cmd /c "jasmine-node ..\server --autotest --watch ..\..\ --captureExceptions --color --verbose"
+start /affinity 0xaa cmd /c "mocha ..\server --autotest --watch ..\..\ --captureExceptions --color --verbose --check-leaks --slow 75"
 wmic process where name="node.exe" CALL setpriority "idle"
--- tests/server/task-spec.js
+++ tests/server/task-spec.js
@@ -1,10 +1,30 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/tasklist-test');
-
 describe('Task model', function () {
+    mongoose.connect('mongodb://localhost:27017/tasklist-test');
+    var conn = mongoose.connection;
+    conn.on('error', function (err) {
+        console.log(err);
+    });
+
+    before(function (done) {
+        /*
+        console.log('NODE_ENV: ' + process.env.NODE_ENV);
+        if (process.env.NODE_ENV !== 'test') {
+            console.log('Sorry, doesn\'t seem like a test env :(');
+            process.exit(1);
+        }
+        */
+        done();
+    });
+
+    after(function (done) {
+        conn.db.dropDatabase(function () {
+            conn.close(done);
+        });
+    });
+
     beforeEach(function () {
-        mongoose.connection.db.dropDatabase();
     });
 
     it('should remove task by ID', function () {
