f62d4f4e5d60bc55b6e574155193833a96090500
John Doe <john@doe.com>
2014-03-10 11:31:46 +0200

    better consts

--- common/common.js
+++ common/common.js
@@ -1,9 +1,11 @@
 var Urls = {};
-Object.defineProperty(Urls, 'ROOT', {value: '/'});
-Object.defineProperty(Urls, 'TASK_LIST', {value: Urls.ROOT + 'tasks/'});
-exports.Urls = Urls;
-
 var Const = {};
 Object.defineProperty(Const, 'TASK_ID_PARAM', { value: 'task_id'});
 
+Object.defineProperty(Urls, 'ROOT', {value: '/'});
+Object.defineProperty(Urls, 'TASK_LIST', {value: Urls.ROOT + 'tasks'});
+Object.defineProperty(Urls, 'TASK', {value: Urls.TASK_LIST + '/:' + Const.TASK_ID_PARAM });
+
 exports.Const = Const;
+exports.Urls = Urls;
+
