8a3c84b1ddeeb9d5ce64c1f4beefd849b7149d31
John Doe <john@doe.com>
2014-03-12 07:39:29 +0200

    Major refactoring in IndexController, the code is much more cleaner and in JS style (using closures instead of states)

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,26 @@
-IndexController = function (Task, User) {
-    this.Task = Task;
-    this.User = User;
-};
-
 /*
  * GET /.
  */
 
-IndexController.prototype.index = function (req, res) {
-    var thisObj = this;
-    this.Task.find({}, function (err, docs) {
-        if (err) { res.send(500, 'Error: ' + err); }
-        thisObj.User.find({}, function (err, users) {
-             if (err) { res.send(500, 'Error: ' + err); } 
-             var usernames = users.map(function (user) { return user.username; });
-             res.render('index.jade', {
-                            title: 'Shared TODO',
-                            tasks: docs,
-                            user: req.user,
-                            users: usernames
-                        });
+module.exports = function(Task, User) {
+    var index = function (req, res) {
+        Task.find({}, function (err, docs) {
+            if (err) { res.send(500, 'Error: ' + err); }
+            User.find({}, function (err, users) {
+                if (err) { res.send(500, 'Error: ' + err); }
+                var usernames = users.map(function (user) { return user.username; });
+                res.render('index.jade', {
+                    title: 'Shared TODO',
+                    tasks: docs,
+                    user: req.user,
+                    users: usernames
+                });
+            });
         });
-        
-    });
+    };
+
+    return {
+        index: index
+    };
 };
 
-exports.IndexController = IndexController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var Urls = require('../common/common').Urls;
-var IndexController = require('../controllers/index').IndexController;
 
 module.exports = function (app, Task, User) {
-    indexController = new IndexController(Task, User);
-    app.get(Urls.ROOT, indexController.index.bind(indexController));
+    indexController = require('../controllers/index')(Task, User);
+    app.get(Urls.ROOT, indexController.index);
     
     require('./tasks')(app, Task);
     require('./user')(app, User);
